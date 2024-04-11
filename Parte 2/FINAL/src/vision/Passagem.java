8c01fbe64092b584419e98a86dc5a0165e7f2213
Patrick Gomes de Oliveira <128257785+PatrickGDO@users.noreply.github.com>
2024-04-11 12:56:59 -0300

    Update Passagem.java

--- Parte 2/FINAL/src/vision/Passagem.java
+++ Parte 2/FINAL/src/vision/Passagem.java
@@ -2,9 +2,14 @@ package vision;
 public class Passagem {
 	private voo Voo;
 	private double preco;
+	private int Cod;
+	private static int QtdTotal = 0;
 
 	public Passagem(voo vooin, double precoin) {
 		this.Voo = vooin;
+		Cod = QtdTotal;
+		QtdTotal++;
+		
 		if(precoin > 0)
 			this.preco = precoin;
 	}
@@ -18,3 +23,4 @@ public class Passagem {
 	}
 
 }
+
